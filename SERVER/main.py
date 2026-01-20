43df19ba136d2db319834568b92f7f8087c3edcd
ayushimaurya546-sv <ayushimaurya546@gmail.com>
2026-01-21 01:17:08 +0530

    PORT Removed

--- SERVER/main.py
+++ SERVER/main.py
@@ -45,6 +45,6 @@ def home():
 
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT"))
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+ #   port = int(os.environ.get("PORT"))
+  #  app.run(host="0.0.0.0", port=port)
